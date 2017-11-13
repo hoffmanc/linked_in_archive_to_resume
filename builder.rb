950f6c4768b7a53c565429e2679097c71287888e
Chris Hoffman <chris.c.hoffman@gmail.com>
2017-11-12 21:21:52 -0500

    initial commit

---
+++ builder.rb
@@ -0,0 +1,33 @@
+#!/usr/bin/env ruby -w
+
+require "rubygems"
+require "haml"
+require "csv"
+
+@profile = CSV.open("data/Profile.csv", headers: true).readlines.first
+
+CSV.open("data/Email Addresses.csv", headers: true).each do |row|
+  if row["Primary"] == "Yes"
+    @primary_email = row["Email Address"]
+  end
+end
+
+@experience_rows = CSV.open("data/Positions.csv", headers: true).readlines.map do |row|
+  row["Description"] = row["Description"].gsub(/\n+/, "</p><p>")
+  row
+end
+@project_rows = CSV.open("data/Projects.csv", headers: true).readlines
+@education_rows = CSV.open("data/Education.csv", headers: true).readlines
+recommendation_rows = CSV.open("data/Recommendations Received.csv", headers: true).readlines
+@recommendations_hash = {}
+recommendation_rows.each do |row|
+  if @recommendations_hash[row["Company"]].nil?
+    @recommendations_hash[row["Company"]] = []
+  end
+  @recommendations_hash[row["Company"]] << row
+end
+
+engine = Haml::Engine.new(open("template.haml").read)
+File.open("resume.html","w") do |ofile|
+  ofile.puts engine.render(self)
+end
